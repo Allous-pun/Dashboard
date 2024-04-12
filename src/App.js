6653181211cc58be99cc265fd663de0f7031254c
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-04-12 12:52:05 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { theme as customTheme } from './theme';
+import ColorModeContext from './components/ColorModeContext';
+import Layout from './layout/Layout';
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+  const [mode, setMode] = useState('dark');
+  const colorMode = useMemo(
+    () => ({
+      // The theme mode switch will invoke this method
+      toggleColorMode: () => {
+        window.localStorage.setItem(
+          'themeMode',
+          mode === 'dark' ? 'light' : 'dark'
+        );
+        setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
+      },
+    }),
+    [mode]
+  );
+
+  useEffect(() => {
+    try {
+      const localTheme = window.localStorage.getItem('themeMode');
+      localTheme ? setMode(localTheme) : setMode('dark');
+    } catch {
+      setMode('dark');
+    }
+  }, []);
+
+  return (
+    <HelmetProvider>
+      <Helmet
+        titleTemplate="%s | Bob's Programming Academy"
+        defaultTitle="Bob's Programming Academy"
+      />
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={customTheme[mode]}>
+          <CssBaseline />
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route exact path='/' element={<Dashboard />} />
+              </Routes>
+            </Layout>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </HelmetProvider>
+  );
+};
+
+export default App;
---
+++ src/components/CustomButton.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
+
+const CustomButton = ({ href, icon, text }) => {
+  const theme = useTheme();
+
+  return (
+    <Button
+      component='a'
+      color='primary'
+      href={href}
+      size='small'
+      variant='text'
+      sx={{
+        color:
+          theme.palette.mode === 'dark'
+            ? theme.palette.text.secondary
+            : theme.palette.text.primary,
+        fontSize: theme.typography.subtitle1,
+        fontWeight: 'medium',
+        marginRight: 2,
+        '&:active': {
+          color:
+            theme.palette.mode === 'dark'
+              ? theme.palette.primary.contrastText
+              : theme.palette.primary.main,
+        },
+        '&:hover': {
+          color:
+            theme.palette.mode === 'dark'
+              ? theme.palette.primary.contrastText
+              : theme.palette.primary.main,
+        },
+        '& svg': {
+          marginRight: 0.5,
+        },
+      }}
+    >
+      {icon} {text}
+    </Button>
+  );
+};
+
+export default CustomButton;
